a43149468ddcd47810d83cd72dcf3baf583938da
nn9416 <nikolanrt9416@gmail.com>
2019-05-09 09:54:26 +0200

    Added User.cs. Customer and Admin classes inherit this class.

---
+++ tvp-1-project/Model/User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace tvp_1_project.Model
+{
+    [Serializable]
+    public class User : ISerializable
+    {
+        [NonSerialized]
+        private string display;
+
+        public User() { }
+
+        public User(string username, string password)
+        {
+            this.Username = username;
+            this.Password = password;
+            this.Display = ToString();
+        }        
+
+        #region ISerializable
+        public User(SerializationInfo info, StreamingContext context)
+        {
+            Username = info.GetString("Username");
+            Password = info.GetString("Password");
+            Display = ToString();
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Username", Username);
+            info.AddValue("Password", Password);
+        }
+        #endregion
+
+        public override string ToString()
+        {
+            return $"{Username}";
+        }
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Display { get => display; private set => display = value; }
+    }
+}
