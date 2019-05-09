eb7e6c21d3cd8801861b73920d3c94aa41e32a40
nn9416 <nikolanrt9416@gmail.com>
2019-05-09 12:42:48 +0200

    Added AppSettings.cs that contains methods for creating app directories for storing data.

---
+++ tvp-1-project/AppSettings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace tvp_1_project
+{
+    class AppSettings
+    {
+        private static readonly string APPDIRECTORY = Environment.ExpandEnvironmentVariables(@"%APPDATA%\tvp1project");
+
+        public static string GetDirectory(string dataDirectory) => $@"{APPDIRECTORY}\{dataDirectory}";
+
+        public static void CreateDirectories()
+        {
+            if (!Directory.Exists(APPDIRECTORY))
+            {
+                Directory.CreateDirectory(APPDIRECTORY);
+                Directory.CreateDirectory(GetDirectory("admin"));
+                Directory.CreateDirectory(GetDirectory("cars"));
+                Directory.CreateDirectory(GetDirectory("customers"));
+                Directory.CreateDirectory(GetDirectory("offers"));
+                Directory.CreateDirectory(GetDirectory("bookings"));
+            }
+        }
+    }
+}
