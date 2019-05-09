4c625b42fa82f9582e0ae1eefa12858a55875b75
nn9416 <nikolanrt9416@gmail.com>
2019-05-09 21:25:28 +0200

    Added CustomEventArgs.cs Using this class allows us to pass custom object as event argument

---
+++ tvp-1-project/BussinesLogic/CustomEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace tvp_1_project.BussinesLogic
+{
+    public class CustomEventArgs : EventArgs
+    {
+        public object Data { get; set; }
+
+        public CustomEventArgs(object data)
+        {
+            Data = data;
+        }
+    }
+}
