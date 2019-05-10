08b4e145817713bae1b6ca19f8164a070f08f362
nn9416 <nikolanrt9416@gmail.com>
2019-05-10 19:57:36 +0200

    Added methods to registration presenter

--- tvp-1-project/Presenter/PRegistration.cs
+++ tvp-1-project/Presenter/PRegistration.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
+using tvp_1_project.Model;
+using tvp_1_project.View;
 
 namespace tvp_1_project.Presenter
 {
-    class PRegistration
+    public class PRegistration
     {
+        private readonly IRegistrationView _view;
+        private readonly ILoginRepository _repository;
+
+        public PRegistration(IRegistrationView view, ILoginRepository repository)
+        {
+            _view = view;
+            _repository = repository;
+            view.Presenter = this;
+        }
+
+        internal bool TryCreateNew(Dictionary<string, string> values)
+        {
+            if (Validation.ValidateCustomer(values))
+            {
+                DateTime birthDate = DateTime.ParseExact(values["BirthDate"], "dd.MM.yyyy.", CultureInfo.InvariantCulture);
+                Customer newCustomer = new Customer(
+                    values["FirstName"],
+                    values["LastName"],
+                    values["Ssn"],
+                    birthDate,
+                    values["PhoneNumber"],
+                    values["Username"],
+                    values["Password"]);
+                newCustomer.Create();
+                return true;
+            }
+            else            
+                return false;            
+        }
     }
 }
