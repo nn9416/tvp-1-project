42ce079fe025e5af8f8ec2a34b6aecaceb662b0d
nn9416 <nikolanrt9416@gmail.com>
2019-05-10 18:35:16 +0200

    Added Authentication.cs Class for comparing username and password to credentials of users.

---
+++ tvp-1-project/BussinesLogic/Authentication.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using tvp_1_project.Model;
+
+namespace tvp_1_project.BussinesLogic
+{
+    public static class Authentication
+    {
+        public static bool Authenticate(string username, string password, List<User> users)
+        {
+            if (CheckUsername(username, password, users))
+            {
+                IsAdmin = username.Equals("admin");
+                CurrentUser = username;
+                return true;
+            }            
+            return false;
+        }
+
+        public static bool CheckUsername(string username, string password, List<User> users)
+        {
+            foreach (User user in users)
+            {
+                if (user.Username.Equals(username))
+                    return CheckPassword(password, user);
+            }
+            // TODO: Notify user that username is non existent
+            return false;
+        }
+
+        public static bool CheckPassword(string password, User user)
+        {
+            if (user.Password.Equals(password))            
+                return true;
+            
+            // TODO: Notify user that password is wrong
+            return false;
+        }
+
+        public static bool IsAdmin { get; private set; }
+        public static string CurrentUser { get; private set; }
+    }
+}
