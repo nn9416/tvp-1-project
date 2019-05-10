377f8d76bb1a8315a0cfd9d8792f54d85c838813
nn9416 <nikolanrt9416@gmail.com>
2019-05-10 18:53:43 +0200

    Changed startup form in Program.cs

--- tvp-1-project/Program.cs
+++ tvp-1-project/Program.cs
@@ -8,9 +8,6 @@ namespace tvp_1_project
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
@@ -19,6 +16,18 @@ namespace tvp_1_project
 
             AppSettings.CreateDirectories();
             AppSettings.CreateAdmin();
+
+            var loginView = new View.FLogin();
+            var loginRepository = new Model.LoginRepository();
+            var loginPresenter = new Presenter.PLogin(loginView, loginRepository);
+
+            if (loginView.ShowDialog() == DialogResult.OK)
+            {
+                var mainView = new View.FMain();
+                var mainRepository = new Model.DataRepository();
+                var mainPresenter = new Presenter.PMain(mainView, mainRepository);
+                Application.Run(mainView);
+            }
 
             Application.Run(new Form1());
         }
