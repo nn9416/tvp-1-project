616d2bac1bf88164ee7b1f546bb7f0f62eb64a01
nn9416 <nikolanrt9416@gmail.com>
2019-05-13 12:24:07 +0200

    Added ICustomersView.cs interface

---
+++ tvp-1-project/View/Pages/Admin/ICustomersView.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using tvp_1_project.Model;
+using tvp_1_project.Presenter.Pages.Admin;
+
+namespace tvp_1_project.View.Pages.Admin
+{
+    public interface ICustomersView
+    {
+        PCustomers Presenter { get; set; }
+        void PopulateDataViewer(List<Customer> data);
+        void UpdateDataViewer(List<Customer> data);
+    }
+}
