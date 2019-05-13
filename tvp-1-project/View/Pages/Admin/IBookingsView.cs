eb4fa0f63a02a9fc319e1625ddc04d62186c4477
nn9416 <nikolanrt9416@gmail.com>
2019-05-13 15:37:57 +0200

    Added IBookingsView.cs interface

---
+++ tvp-1-project/View/Pages/Admin/IBookingsView.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using tvp_1_project.Model;
+using tvp_1_project.Presenter.Pages.Admin;
+
+namespace tvp_1_project.View.Pages.Admin
+{
+    public interface IBookingsView
+    {
+        PBookings Presenter { get; set; }
+        void PopulateDataViewer(List<Booking> data);
+        void UpdateDataViewer(List<Booking> data);
+        void SetCustomersComboBox(List<Customer> customers);
+        void SetCarsComboBox(List<Car> list);
+    }
+}
