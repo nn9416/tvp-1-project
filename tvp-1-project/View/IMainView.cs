9e56250d34702adddccae9f4215fcc5101d31f57
nn9416 <nikolanrt9416@gmail.com>
2019-05-13 18:40:41 +0200

    Added IMainView.cs interface Added IMainView.cs interface and added using for Presenter in IRegistrationView.cs

---
+++ tvp-1-project/View/IMainView.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+using tvp_1_project.Presenter;
+
+namespace tvp_1_project.View
+{
+    public interface IMainView
+    {
+        PMain Presenter { get; set; }
+        Dictionary<string, UserControl> Pages { get; set; }
+        void SetViewState(bool isAdmin);
+    }
+}
