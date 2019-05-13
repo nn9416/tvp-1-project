a03dd94ae6ff77c224e693ccf9253d2d71d4e3bb
nn9416 <nikolanrt9416@gmail.com>
2019-05-13 13:49:05 +0200

    Added PageOffers.cs

---
+++ tvp-1-project/View/Pages/Admin/PageOffers.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace tvp_1_project.View.Pages.Admin
+{
+    partial class PageOffers
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ucDataViewer = new tvp_1_project.View.UserControls.UCDataViewer();
+            this.SuspendLayout();
+            // 
+            // ucDataViewer
+            // 
+            this.ucDataViewer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(48)))), ((int)(((byte)(82)))));
+            this.ucDataViewer.Font = new System.Drawing.Font("Consolas", 8.25F);
+            this.ucDataViewer.ForeColor = System.Drawing.Color.LightGray;
+            this.ucDataViewer.InputControls = null;
+            this.ucDataViewer.Location = new System.Drawing.Point(0, 0);
+            this.ucDataViewer.MinimumSize = new System.Drawing.Size(900, 590);
+            this.ucDataViewer.Name = "ucDataViewer";
+            this.ucDataViewer.Size = new System.Drawing.Size(900, 590);
+            this.ucDataViewer.TabIndex = 0;
+            this.ucDataViewer.ButtonClick += new System.EventHandler<tvp_1_project.BussinesLogic.CustomEventArgs>(this.UcDataViewerButtons_Click);
+            this.ucDataViewer.Load += new System.EventHandler(this.UcDataViewer_Load);
+            // 
+            // PageOffers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(48)))), ((int)(((byte)(82)))));
+            this.Controls.Add(this.ucDataViewer);
+            this.Font = new System.Drawing.Font("Consolas", 8.25F);
+            this.ForeColor = System.Drawing.Color.White;
+            this.MinimumSize = new System.Drawing.Size(900, 590);
+            this.Name = "PageOffers";
+            this.Size = new System.Drawing.Size(900, 590);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private UserControls.UCDataViewer ucDataViewer;
+    }
+}
