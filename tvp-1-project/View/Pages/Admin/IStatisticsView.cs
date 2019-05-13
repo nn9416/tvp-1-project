b9d0ce1e4bad753f98c31dea744c0e76c6ee5d75
nn9416 <nikolanrt9416@gmail.com>
2019-05-13 18:35:08 +0200

    Added IStatisticsView.cs interface

---
+++ tvp-1-project/View/Pages/Admin/IStatisticsView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tvp_1_project.Presenter.Pages.Admin;
+
+namespace tvp_1_project.View.Pages.Admin
+{
+    public interface IStatisticsView
+    {
+        PStatistics Presenter { get; set; }
+    }
+}
