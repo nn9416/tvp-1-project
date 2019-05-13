c28ec601d9699ce63e1c8c2821bee669959c1f3a
nn9416 <nikolanrt9416@gmail.com>
2019-05-13 18:37:51 +0200

    Added PageStatistics.cs user control

---
+++ tvp-1-project/View/Pages/Admin/PageStatistics.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace tvp_1_project.View.Pages.Admin
+{
+    partial class PageStatistics
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // PageStatistics
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(48)))), ((int)(((byte)(82)))));
+            this.Font = new System.Drawing.Font("Consolas", 8.25F);
+            this.ForeColor = System.Drawing.Color.White;
+            this.MinimumSize = new System.Drawing.Size(900, 590);
+            this.Name = "PageStatistics";
+            this.Size = new System.Drawing.Size(900, 590);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ tvp-1-project/View/Pages/Admin/PageStatistics.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using tvp_1_project.Presenter.Pages.Admin;
+
+namespace tvp_1_project.View.Pages.Admin
+{
+    public partial class PageStatistics : UserControl, IStatisticsView
+    {
+        public PageStatistics()
+        {
+            InitializeComponent();
+        }
+
+        #region IStatisticsView
+        public PStatistics Presenter { get; set; }
+        #endregion
+    }
+}
