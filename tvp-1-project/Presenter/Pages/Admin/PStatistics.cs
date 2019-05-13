d4f63bf92cc630dbed62b700b5723fd3017df433
nn9416 <nikolanrt9416@gmail.com>
2019-05-13 18:36:00 +0200

    Added PStatistics.cs presenter

---
+++ tvp-1-project/Presenter/Pages/Admin/PStatistics.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tvp_1_project.Model;
+using tvp_1_project.View.Pages.Admin;
+
+namespace tvp_1_project.Presenter.Pages.Admin
+{
+    public class PStatistics
+    {
+        private readonly IStatisticsView _view;
+        private readonly IDataRepository _repository;
+
+        public PStatistics(IStatisticsView view, IDataRepository repository)
+        {
+            _view = view;
+            _repository = repository;
+            view.Presenter = this;
+        }
+    }
+}
