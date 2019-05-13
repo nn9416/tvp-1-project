8e52f02bb039a9d92f8c3529bd7481c435bdbd9e
nn9416 <nikolanrt9416@gmail.com>
2019-05-13 10:11:00 +0200

    Added ICarsView.cs interface

---
+++ tvp-1-project/View/Pages/Admin/ICarsView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tvp_1_project.View.Pages.Admin
+{
+    interface ICarsView
+    {
+    }
+}
